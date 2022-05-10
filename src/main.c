565c7ac5eee370bc86f829a5337b761ed76b2080
FrajerTeAge <joe1000@email.cz>
2022-05-10 15:19:33 +0200

    first and final

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
+#include "stm8_hd44780.h"
 #include "stm8s.h"
 #include "milis.h"
 
-//#include "delay.h"
-//#include <stdio.h>
-//#include "uart1.h"
+#include "stm8s_gpio.h"
+#include "stm8s_tim2.h"
+#include "swspi.h"
+
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -12,10 +14,11 @@
 #define LED_PIN  GPIO_PIN_5
 #define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
 #define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
+#define LED_TOGG GPIO_WriteReverse(LED_PORT, LED_PIN)
 
 #define BTN_PORT GPIOE
 #define BTN_PIN  GPIO_PIN_4
+
 #define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
 
 
@@ -23,30 +26,32 @@ void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-
+    GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_SLOW);
     init_milis();
-    //init_uart1();
 }
 
+int a=1000;
+uint16_t b=20;
+int32_t ted=0;
 
-int main(void)
-{
-    uint32_t time = 0;
+int main(void){
 
     setup();
 
+    TIM2_TimeBaseInit(TIM2_PRESCALER_16,20000);
+    TIM2_OC1Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE,1000,TIM2_OCPOLARITY_HIGH);
+    
+    TIM2_OC1PreloadConfig(ENABLE);
+    TIM2_Cmd(ENABLE);
+
     while (1) {
-
-        if (milis() - time > 333 && !BTN_PUSH) {
-            LED_REVERSE; 
-            time = milis();
-            //printf("%ld\n", time);
+    
+        if (BTN_PUSH &&(milis()-ted>=100)) {
+            ted=milis();
+            TIM2_SetCompare1(a);
+            a += b;
         }
-
-        //LED_REVERSE; 
-        //delay_ms(333);
-        //printf("Funguje to!!!\n");
+        if(a>=2000||a<=1000){b=b*-1;}
     }
 }
 
